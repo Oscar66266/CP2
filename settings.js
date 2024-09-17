8e48bd9c1d0b54621a504712184f3062cf2fa6b5
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2024-09-16 19:09:21 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@ global.confirmCode = ''
 
 global.owner = [
    ['5219812796095', 'Kevs  ⭐', true],
-   ['5213142183828', 'Akutawa ⭐', true],
+   ['523142183828', 'Akutawa ⭐', true],
    ['51910108980', '@Samush$_ ⭐', false],
    ['5351524614']
 ]
