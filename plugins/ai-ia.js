27a19847a95c3a99a320db9b8be949ab65aeee44
Oscar66266 <oscarabrahamcabreramarin098@gmail.com>
2024-09-16 22:19:17 -0500

    Update ai-ia.js

--- plugins/ai-ia.js
+++ plugins/ai-ia.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
 const username = `${conn.getName(m.sender)}`
-const basePrompt = `Tu nombre es YaemoriBot-MD y parece haber sido creado por DevDiego. Tu versión actual es 2.0.5, Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
+const basePrompt = `Tu nombre es Yuki y parece haber sido creado por Kev's.Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, y te encanta aprender. Lo más importante es que debes ser amigable con la persona con la que estás hablando. ${username}`
 if (isQuotedImage) {
 const q = m.quoted
 const img = await q.download?.()
@@ -44,7 +44,7 @@ export default handler
 // Función para enviar una imagen y obtener el análisis
 async function fetchImageBuffer(content, imageBuffer) {
 try {
-const response = await axios.post('https://lumin-ai.xyz/', {
+const response = await axios.post('https://Luminai.my.id', {
 content: content,
 imageBuffer: imageBuffer 
 }, {
@@ -58,7 +58,7 @@ throw error }}
 // Función para interactuar con la IA usando prompts
 async function luminsesi(q, username, logic) {
 try {
-const response = await axios.post("https://lumin-ai.xyz/", {
+const response = await axios.post("https://Luminai.my.id", {
 content: q,
 user: username,
 prompt: logic,
